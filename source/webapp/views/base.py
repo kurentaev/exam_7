779b7beeed62837306a060eda53bf483e8733387
Adlet Kurentaev <kurentaev@gmail.com>
2022-10-01 12:28:59 +0600

    Добавлен функционал для отображения записей со статусом Active, записи отсортированы по дате создания

--- source/webapp/views/base.py
+++ source/webapp/views/base.py
@@ -4,7 +4,7 @@ from webapp.models import StatusChoices
 
 
 def index_view(request):
-    guests = GuestsList.objects.all()
+    guests = GuestsList.objects.filter(status="active").order_by("-created_at")
     context = {
         "guests": guests,
         "choices": StatusChoices.choices
